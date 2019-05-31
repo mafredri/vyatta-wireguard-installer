5e6f2f2c87f70ce06f9c3e186eba566bfaf7cf09
Mathias Fredriksson <mafredri@gmail.com>
2019-05-31 19:24:08 +0300

    Add Edgerouter X SFP (e51) to supported boards

--- wireguard.sh
+++ wireguard.sh
@@ -4,6 +4,7 @@ set -e
 declare -A SUPPORTED_BOARDS
 SUPPORTED_BOARDS=(
 	[e50]=e50 # ER-X (EdgeRouter X)
+	[e51]=e50 # Edgerouter X SFP
 	[e101]=e100 # ERLite-3 (EdgeRouter Lite 3-Port)
 	[e102]=e100 # ERPoe-5 (EdgeRouter PoE 5-Port)
 	[e200]=e200 # EdgeRouter Pro 8-Port
