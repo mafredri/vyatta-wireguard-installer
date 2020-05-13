367ab361ffbbe1905a548dbacfc37edcf9c1cfb3
Mathias Fredriksson <mafredri@gmail.com>
2020-05-13 21:49:22 +0300

    Bump the version to 1.3.1

--- wireguard.sh
+++ wireguard.sh
@@ -8,7 +8,7 @@ fi
 # The repository from which we fetch new releases.
 WIREGUARD_REPO=WireGuard/wireguard-vyatta-ubnt
 
-WIREGUARD_INSTALLER_VERSION=1.3.0
+WIREGUARD_INSTALLER_VERSION=1.3.1
 
 declare -A SUPPORTED_BOARDS
 SUPPORTED_BOARDS=(
